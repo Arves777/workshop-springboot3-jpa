cb33e8c2a428fec9a1cc80967319186f25f35a0d
Arves777 <adrianrosselis2017@gmail.com>
2023-07-17 12:56:54 -0300

    User entity and resource

---
+++ src/main/java/com/arves/course/resources/UserResources.java
@@ -0,0 +1,18 @@
+package com.arves.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.arves.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"pedro","pedro@hotmail.com","987543222","250694");
+		return ResponseEntity.ok().body(u);
+	}
+}
