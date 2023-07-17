e84e5865e0489df9aa38ad9bce08ef548af4d416
Arves777 <adrianrosselis2017@gmail.com>
2023-07-17 12:06:36 -0300

    Project created

---
+++ src/test/java/com/arves/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arves.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
