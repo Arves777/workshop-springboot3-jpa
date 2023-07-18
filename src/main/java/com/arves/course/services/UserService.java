937ca078707266335de0651c6417675da2c69f7c
Arves777 <adrianrosselis2017@gmail.com>
2023-07-18 13:47:20 -0300

    Service layer, components registration

---
+++ src/main/java/com/arves/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.arves.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arves.course.entities.User;
+import com.arves.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> optional = repository.findById(id);
+		return optional.get();
+	}
+}
