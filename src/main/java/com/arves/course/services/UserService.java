d534985a364a10594bc1ee6bfdd2c6cfa63c448a
Arves777 <adrianrosselis2017@gmail.com>
2023-07-25 12:21:55 -0300

    Exception handling - delete

--- src/main/java/com/arves/course/services/UserService.java
+++ src/main/java/com/arves/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.arves.course.entities.User;
 import com.arves.course.repositories.UserRepository;
+import com.arves.course.services.exceptions.DatabaseException;
 import com.arves.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,7 +32,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);;
+		try {
+		repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User user) {
